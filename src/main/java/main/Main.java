bf146658f9fd6ee1dc10f5e466acece59b871636
Hepha <trantrunghieudo@gmail.com>
2025-03-04 11:11:52 +0700

    Start

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
 //        User newUser = new User("admin2", "admin123", "Quản trị viên", "admin2@example.com", "0223456789", "ADMIN");
 //        userDAO.addUser(newUser);
         UserRepository repo=new UserRepository();
-        Optional<User> user = repo.findByUsername("Hepha2");
+        Optional<User> user = repo.findByUsername("Hepha");
         System.out.println(user.get().getPassword());
         System.out.println(user.isPresent() && new UserService(repo).verifyPassword("1234", user.get().getPassword()));
     }
